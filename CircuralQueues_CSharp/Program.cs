b9aee076f38acf93a62b1a84ff9bd77b78e49bce
FaqodAqomaddin <faqod.aqomaddin.ft21@mail.umy.ac.id>
2022-12-16 17:59:50 +0700

    add method insert

--- CircuralQueues_CSharp/Program.cs
+++ CircuralQueues_CSharp/Program.cs
@@ -13,5 +13,31 @@ namespace CircuralQueues_CSharp
             FRONT = -1;
             REAR = -1;
         }
+        public void insert(int element)
+        {
+            /*this statement checks for the overflow condition. */
+            if ((FRONT == 0 && REAR == 0) || (FRONT == REAR + 1))
+            {
+                Console.WriteLine("/nQueue overflow\n");
+                return;
+            }
+            /* the following statement checks whether the queue is empty. if the queue is empty, then the value of the REAR and FRONT variables is set to 0. */
+            if (FRONT == - 1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+            else
+            {
+                /*if REAR is at the last position of the array, then the value of REAR is set to 0 that coresponds to the first position of the array. */
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    /*if REAR is not at the last position, then its value is incremented by one.*/
+                    REAR = REAR + 1;
+            }
+            /*Once the position of REAR is determined, the element is added at its proper place */
+            queue_array[REAR] = element;
+        }
     }
 }
